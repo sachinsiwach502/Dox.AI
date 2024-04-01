1fd27ab14d62d9bcac2c74e8a75206ba065410d0
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-01 10:24:19 +0530

    complete the slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,47 @@ let menuList = document.querySelector(".menubar");
 
 menuIcon.addEventListener("click", () => {
     menuList.classList.toggle("show")
-})
+});
+
+$('.slick-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 2000,
+
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    focusOnSelect: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
