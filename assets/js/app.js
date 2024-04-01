364006e22ce9a66faaa526c2b22e471e79caae47
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-01 15:45:59 +0530

    make slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,44 +42,31 @@ $('.slick-slider').slick({
                 slidesToScroll: 1
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
-
 $('.slick_slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
+    centerMode: true,
+    centerPadding: '60px',
     slidesToShow: 1,
-    slidesToScroll: 1,
+    dots: true,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 768,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
             }
         },
         {
             breakpoint: 480,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
