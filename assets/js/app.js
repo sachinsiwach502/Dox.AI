84f17fc3eda92ebbd981448d02d5e28b9013c0c6
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-04 10:34:20 +0530

    make the foote like no-wrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
 let menuIcon = document.querySelector(".menuIcon");
 let menuList = document.querySelector(".menubar");
 
-
-
 menuIcon.addEventListener("click", () => {
     menuList.classList.toggle("show");
 });
